83d41cffc9296158579542ee16e8900c51f9b1ac
marlon20161188f <mavilat@uni.pe>
2021-09-28 05:45:07 -0500

    config url

--- Admin/config/constantes.php
+++ Admin/config/constantes.php
@@ -1,5 +1,6 @@
 <?php
     //$url_site = 'http://deliveryapp.me/';
     //$url_site = 'http://localhost/Admin/';
-    $url_site = 'http://Adminapp.me/';
+    //$url_site = 'http://Adminapp.me/';
+    $url_site = 'https://admin-vehiculo.herokuapp.com/';
 ?>
