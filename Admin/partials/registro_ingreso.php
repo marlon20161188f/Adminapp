bb27f5c62dcadd6c93d624e16227c2ab19edb22e
marlon20161188f <mavilat@uni.pe>
2021-09-29 01:29:29 -0500

    ssssddd

--- Admin/partials/registro_ingreso.php
+++ Admin/partials/registro_ingreso.php
@@ -38,12 +38,12 @@
                                         <td><?php echo $item['estacionamiento']; ?></td>
                                         <td><?php echo $item['fecha']; ?></td>
                                         <td>
-                                            <button  id="btn_<?php echo $item['id']; ?>" class="btn btn-secondary btn-sm btn-circle margin" type="hidden" onclick="editModal(<?php echo $item['id']; ?>);" data-id="<?php echo $item['id']; ?>" data-placa="<?php echo $item['placa']; ?>"data-marca="<?php echo $item['marca']; ?>" data-color="<?php echo $item['color']; ?>"data-estacionamiento="<?php echo $item['estacionamiento']; ?>"data-fecha="<?php echo $item['fecha']; ?>"> <!-- data-estado="<?php //echo $item['id_provedor']; ?>"-->
-                                                <span class="fa fa-pencil-alt"></span>
-                                            </button>
-                                            <button type="hidden" class="btn btn-secondary btn-sm btn-circle margin" onclick="Eliminar(<?php echo $item['id']; ?>)">
-                                                <span class="fa fa-trash"></span>
-                                            </button>
+                                            <input  id="btn_<?php echo $item['id']; ?>" class="btn btn-secondary btn-sm btn-circle margin" type="hidden" onclick="editModal(<?php echo $item['id']; ?>);" data-id="<?php echo $item['id']; ?>" data-placa="<?php echo $item['placa']; ?>"data-marca="<?php echo $item['marca']; ?>" data-color="<?php echo $item['color']; ?>"data-estacionamiento="<?php echo $item['estacionamiento']; ?>"data-fecha="<?php echo $item['fecha']; ?>"> <!-- data-estado="<?php //echo $item['id_provedor']; ?>"-->
+                                                <!-- <span class="fa fa-pencil-alt"></span> -->
+                                            
+                                            <input type="hidden" class="btn btn-secondary btn-sm btn-circle margin" onclick="Eliminar(<?php echo $item['id']; ?>)">
+                                                <!-- <span class="fa fa-trash"></span> -->
+                                            
                                         </td>
                                      </tr>
                                 <?php } ?>
