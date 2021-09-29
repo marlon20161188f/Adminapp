227873660a8a08edefc18e7d6fd3d104767f8b24
marlon20161188f <mavilat@uni.pe>
2021-09-29 04:30:35 -0500

    ccv

--- Admin/layout/sidebar.php
+++ Admin/layout/sidebar.php
@@ -5,7 +5,7 @@
 				<ul class="nav nav-pills flex-column sidebar-nav">
                     <?php
                     if($usuario->id_tipo_usuario == '1'){
-                        echo '<li class="nav-item"><a class="nav-link active" href="<?php echo $url_site; ?>index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
+                        echo '<li class="nav-item"><a class="nav-link active" href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
 					 <li class="nav-item"><a class="nav-link" href="'.$url_site.'registrovehicular/"><em class="fa fa-database"></em> Vehículos registrados</a></li>
                     <li class="nav-item"><a class="nav-link" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> REGISTRO DE INGRESO</a></li>';
                     }
