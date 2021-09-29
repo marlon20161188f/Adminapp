0e09120eb1fd8d881e69e81940398172335427d5
marlon20161188f <mavilat@uni.pe>
2021-09-29 04:15:12 -0500

    Update main-header.php

--- Admin/layout/main-header.php
+++ Admin/layout/main-header.php
@@ -193,7 +193,7 @@
 	</ul>
 </nav>
 <div class="col-md-6 col-lg-8" >
-		<h1 type="hidden" class="float-left text-center text-md-center"></h1>
+		<h1 type="hidden" class="float-left text-center text-md-center">Administrador de ingreso vehicular</h1>
 	</div>
 
 	<div class="clear"></div>
