280b1099f5075c8f57d5f2694c1112797b2c12d4
marlon20161188f <mavilat@uni.pe>
2021-10-01 03:25:00 -0500

    button

--- Admin/layout/sidebar.php
+++ Admin/layout/sidebar.php
@@ -158,19 +158,19 @@
 				<ul class="nav nav-pills flex-column sidebar-nav">
                     <?php
                     if($usuario->id_tipo_usuario == '1'){//OPERADOR
-                        echo '<li class="nav-item"><a class="nav-link " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
-                    <li class="nav-item"><a class="nav-link" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>';
+                        echo '<li class="nav-item"><a class="logout-button " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
+                    <li class="nav-item"><a class="logout-button" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>';
                     }
 					if($usuario->id_tipo_usuario == '2'){//SUPERVISOR
-                        echo '<li class="nav-item"><a class="nav-link " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
-					 <li class="nav-item"><a class="nav-link" href="'.$url_site.'registrovehicular/"><em class="fa fa-database"></em> Vehículos registrados</a></li>
-                    <li class="nav-item"><a class="nav-link" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>';
+                        echo '<li class="nav-item"><a class="logout-button " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
+					 <li class="nav-item"><a class="logout-button" href="'.$url_site.'registrovehicular/"><em class="fa fa-database"></em> Vehículos registrados</a></li>
+                    <li class="nav-item"><a class="logout-button" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>';
 				}
 					if($usuario->id_tipo_usuario == '3'){ //ADMINISTRADOR
-						 echo '<li class="nav-item"><a class="nav-link " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
-						<li class="nav-item"><a class="nav-link" href="'.$url_site.'registrovehicular/"><em class="fa fa-database"></em> Vehículos registrados</a></li>
-					   <li class="nav-item"><a class="nav-link" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>
-					   <li class="nav-item"><a class="nav-link" href="'. $url_site.'vehiculo/"><em class="fa fa-database"></em> Gestor de usuarios</a></li>';  
+						 echo '<li class="nav-item"><a class="logout-button " href="'.$url_site.'index.php"><em class="fa fa-address-book"></em> Consulta</a></li>
+						<li class="nav-item"><a class="logout-button" href="'.$url_site.'registrovehicular/"><em class="fa fa-database"></em> Vehículos registrados</a></li>
+					   <li class="nav-item"><a class="logout-button" href="'. $url_site.'registroingresos/"><em class="fa fa-database"></em> Historial de ingreso</a></li>
+					   <li class="nav-item"><a class="logout-button" href="'. $url_site.'vehiculo/"><em class="fa fa-database"></em> Gestor de usuarios</a></li>';  
                     }
                     ?>
 
