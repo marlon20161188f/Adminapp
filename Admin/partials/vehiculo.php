5dc04ea7e6fedd9940c5b94f01cebee514b33031
marlon20161188f <mavilat@uni.pe>
2021-10-04 17:59:02 -0500

    Update vehiculo.php

--- Admin/partials/vehiculo.php
+++ Admin/partials/vehiculo.php
@@ -117,11 +117,10 @@
                 <div class="col-12 no-padding">
                     <!-- <input type="hidden" name="id" id="id">
                     <input type="hidden" name="option" value="U"> -->
-                   <select name="id_tipo_usuario" placeholder="Ingrese la descripción del usuario">
-                   <option  ></option>
-                   <option  name="id_tipo_usuario" id="id_tipo_usuario" value="1">Operador</option>
-                   <option  name="id_tipo_usuario" id="id_tipo_usuario" value="2">Supervisor</option>
-                   <option  name="id_tipo_usuario" id="id_tipo_usuario" value="3">Administrador</option>
+                   <select name="id_tipo_usuario" placeholder="Elije la descripción del usuario">
+                   <option  name="id_tipo_usuario"  value="1">Operador</option>
+                   <option  name="id_tipo_usuario"  value="2">Supervisor</option>
+                   <option  name="id_tipo_usuario"  value="3">Administrador</option>
                   </select>                              
                 </div>
             </div>
